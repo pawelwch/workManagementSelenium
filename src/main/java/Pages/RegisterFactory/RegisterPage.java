0f38d6793840b155fd90dfbd05caf8341670474f
Emil Czerski <emil.czerski@l4a-soft.com>
2019-03-26 12:11:49 +0100

    Dodanie metod z rejestracja

--- src/main/java/Pages/RegisterFactory/RegisterPage.java
+++ src/main/java/Pages/RegisterFactory/RegisterPage.java
@@ -4,29 +4,33 @@ import TestMethods.ConfigureMethods;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import static TestMethods.ConfigureMethods.sleep;
 import static TestMethods.ConfigureMethods.waitForIt;
 
 
-/*
-klasa RegisterPage - klasa w której przechowujemy i wykonujemy metody dot. RegisterPage. Roszerzona jest o klasę RegisterWebElements,
+/**klasa RegisterPage - klasa w której przechowujemy i wykonujemy metody dot. RegisterPage. Roszerzona jest o klasę RegisterWebElements,
 dzięki której będziemy się odwoływać do poszczególnych elementów strony.
  */
 public class RegisterPage extends RegisterWebElements{
 
-    /*
-    Metoda przekazująca w parametrze zmienna driver która jest obiektu typu WebDriver. Metoda super() powoduje wywołanie konstruktora klasy nadrzędnej.
+    /** Metoda przekazująca w parametrze zmienna driver która jest obiektu typu WebDriver. Metoda super() powoduje wywołanie konstruktora klasy nadrzędnej.
     W tym przypadku metoda RegisterPage korzysta z konstruktora utworzonego w klasie RegisterWebElements;
      */
     public RegisterPage(WebDriver driver) {
         super(driver);
     }
 
-    /*
-        Definiujemy metodę przekazująca w parametrze wartość typu String.
+    /**Definiujemy metodę przekazująca w parametrze wartość typu String.
         Wykonanie metody z parametrem spowoduje przekazanie parametru do elementu strony registerPhoneInput oraz kliknięcie submitFirstView;
      */
+    public void selectCountry() {
+        sleep(1000);
+        waitForIt(countrySelector).click();
+        waitForIt(polishPrefix).click();
+    }
+
     public void registerCompletePhone(String phone) {
       waitForIt(registerPhoneInput).sendKeys(phone);
         waitForIt(submitFirstView).click();
@@ -35,13 +39,83 @@ public class RegisterPage extends RegisterWebElements{
 
     public void confirmSms(String sms) {
 //        waitForIt(smsInput).sendKeys(sms);
+        sleep(1000);
         waitForIt(confirmSmsCodeButton).click();
     }
 
     public void setPinView(String pin1, String pin2) {
         waitForIt(firstPinInput).sendKeys(pin1);
         waitForIt(secondPinInput).sendKeys(pin2);
+        sleep(1500);
         waitForIt(pinConfirmButton).click();
     }
+
+    public void accountTypeSelector(WebElement accountType) {
+        waitForIt(accountType).click();
+    }
+
+
+    //selektory z datą do zparametryzowania.
+    public void setUpAccount(String fullName, String email,String year, String month, String day, String address, boolean policies, boolean messages) {
+        waitForIt(this.fullName).sendKeys(fullName);
+        waitForIt(this.email).sendKeys(email);
+        waitForIt(yearInput).click();
+        waitForIt(year2000).click();
+        waitForIt(month1).click();
+        waitForIt(day20).click();
+        waitForIt(citySelector).click();
+        waitForIt(firstCityInSelektor).sendKeys("warsz");
+        waitForIt(WarsawCity).click();
+        waitForIt(this.address).sendKeys(address);
+        if (policies==true) {
+            waitForIt(this.policies).click();
+        }
+        if (messages==true) {
+            waitForIt(this.messages).click();
+        }
+        waitForIt(confirmSetupAccount).click();
+    }
+
+    public void moreAboutYourselfView(WebElement gender, String height, String weight, WebElement bloodType) {
+        waitForIt(gender).click();
+        waitForIt(this.height).sendKeys(height);
+        waitForIt(this.weight).sendKeys(weight);
+        waitForIt(bloodType).click();
+        waitForIt(skillsets).click();
+        waitForIt(skillExcell).click();
+        waitForIt(gender).click();
+        waitForIt(licensesAndCertificates).click();
+        waitForIt(licenseOne).click();
+        waitForIt(education).click();
+        waitForIt(educationVol1).click();
+        waitForIt(languages).click();
+        waitForIt(languageEnglish).click();
+        waitForIt(continueButtonMoreAboutU).click();
+        sleep(2000);
+    }
+
+    public void photoUpload (String action) {
+        switch (action) {
+            case "skip":
+                waitForIt(skipButton).click();
+                break;
+            case "browser":
+                waitForIt(browserGallery).click();
+                break;
+            case "takePhoto":
+                waitForIt(takeAPhoto).click();
+                break;
+        }
+    }
+
+    public void documentsUploadView (String uploadIdCard, String uploadCertificates, String idCard) {
+        waitForIt(this.uploadIdCard).click();
+        waitForIt(this.uploadCertificates).click();
+        waitForIt(this.uploadIdCard).sendKeys(idCard);
+    }
+
+
+
+
 
 }
--- src/main/java/Pages/RegisterFactory/RegisterWebElements.java
+++ src/main/java/Pages/RegisterFactory/RegisterWebElements.java
@@ -19,6 +19,7 @@ public class RegisterWebElements {
 
     // Lokalizujemy wszystkie elementy widoku RegisterPage za pomocą adnotacji @Findby;
     @FindBy(xpath = "//*[@type='tel']") WebElement registerPhoneInput;
+
     @FindBy(className = "selected-flag")WebElement countrySelector;
     @FindBy(xpath = "//*[@data-country-code='pl']") WebElement polishPrefix;
     @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[2]/div[2]/div/button") WebElement submitFirstView;
@@ -31,17 +32,65 @@ public class RegisterWebElements {
     @FindBy(xpath = "//*[.='Labourer']") public static WebElement labourer;
     @FindBy(xpath = "//*[.='Contractor']") public static WebElement contractor;
     @FindBy(xpath = "//*[.='Employer']") public static WebElement employer;
+
+//selektgory z datą do zparametryzowania!!
     @FindBy(id = "fullName")  WebElement fullName;
     @FindBy(id = "email")  WebElement email;
     @FindBy(id = "dropdown-year")  WebElement yearInput;
-    @FindBy(className = "month-input")  WebElement monthInput;
-    @FindBy(className = "day-input")  WebElement dayInput;
-    @FindBy(className = "css-vj8t7z") WebElement countrySelect;
-    @FindBy(className = "css-wqgs6e")WebElement afganistan; //Lub  @FindBy(id = "react-select-2-option-0")WebElement afganistan;
-    @FindBy(className = "css-1hwfws3")  WebElement citySelector;
+    @FindBy (xpath = "//*[@value='2000']") WebElement year2000;
+    @FindBy(xpath = "//*[.='January']")  WebElement month1;
+    @FindBy(xpath = "//*[@value='20']")  WebElement day20;
+
+    @FindBy(className = "css-1hwfws3") WebElement countrySelect;
+    @FindBy(className = "css-11unzgr") WebElement firstCountryInSelector;
+    @FindBy(className = "css-1g6gooi") WebElement countryInput;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div/div[5]/div/div[1]/div[2]")  WebElement citySelector;
+    @FindBy(id = "react-select-3-input")  WebElement firstCityInSelektor;
+    @FindBy(id = "react-select-3-option-0") WebElement WarsawCity;
     @FindBy(id = "address")  WebElement address;
     @FindBy(id = "policies")  WebElement policies;
-    @FindBy(xpath = "messages")  WebElement messages;
-    @FindBy(xpath = "//*[.='Continue']") WebElement confirmSetupAccount;
+    @FindBy(id = "messages")  WebElement messages;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div/div[9]/button") WebElement confirmSetupAccount;
+
+    @FindBy(id = "male" )public static WebElement male;
+    @FindBy(id = "female" )public static WebElement female;
+    @FindBy(id = "other" )public static WebElement other;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[2]/div/label/input" ) WebElement height;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[3]/div/label/input" ) WebElement weight;
+    @FindBy(id = "a+" )public static WebElement a_plus;
+    @FindBy(id = "a-" )public static WebElement a_minus;
+    @FindBy(id = "b+" )public static WebElement b_plus;
+    @FindBy(id = "b-" )public static WebElement b_minus;
+    @FindBy(id = "ab+" )public static WebElement ab_plus;
+    @FindBy(id = "ab-" )public static WebElement ab_minus;
+    @FindBy(id = "0+" )public static WebElement zero_plus;
+    @FindBy(id = "0-" )public static WebElement zero_minus;
+    @FindBy(className = "css-1hwfws3" ) WebElement skillsets;
+    @FindBy(id = "react-select-2-option-0")public static WebElement skillExcell;
+    @FindBy(id = "react-select-2-option-1")public static WebElement skillMsOffice;
+    @FindBy(id = "react-select-2-option-2")public static WebElement skillProgramming;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[6]/div/div/div[1]" ) WebElement licensesAndCertificates;
+    @FindBy(id = "react-select-3-option-0" )public static WebElement licenseOne;
+    @FindBy(id = "react-select-3-option-1" )public static WebElement licenseTwo;
+    @FindBy(id = "react-select-3-option-2" )public static WebElement licenseThree;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[7]/div/div/div[1]" ) WebElement education;
+    @FindBy(id = "react-select-4-option-0" )public static WebElement educationVol1;
+    @FindBy(id = "react-select-4-option-1" )public static WebElement educationVol2;
+    @FindBy(id = "react-select-4-option-2" )public static WebElement educationVol3;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[8]/div/div/div[1]" ) WebElement languages;
+    @FindBy(id = "react-select-5-option-0" )public static WebElement languagePolish;
+    @FindBy(id = "react-select-5-option-1" )public static WebElement languageEnglish;
+    @FindBy(id = "react-select-5-option-2" )public static WebElement languageGerman;
+    @FindBy(id = "react-select-5-option-3" )public static WebElement languageHindi;
+    @FindBy(id = "react-select-5-option-4" )public static WebElement languageSpanish;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[9]/button") WebElement continueButtonMoreAboutU;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[1]/button") WebElement skipButton;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[3]/div[1]/div/button") WebElement browserGallery;
+    @FindBy (xpath = "//*[@id=\"app\"]/div[2]/div/div[3]/div[2]/button") WebElement takeAPhoto;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[1]/div[3]/div/input") WebElement uploadIdCard;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[2]/div[3]/div/input") WebElement uploadCertificates;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[2]/div/div[3]/div[2]/p[2]/input") WebElement idNumber;
 
 }
